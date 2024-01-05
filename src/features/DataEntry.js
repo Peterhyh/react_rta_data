2ac37f0ea7310128a592280ba359ed608a896b9f
Peter Huynh <peterhyh@yahoo.com>
2024-01-04 20:30:40 -0600

    progyny form setup

--- src/features/DataEntry.js
+++ src/features/DataEntry.js
@@ -2,17 +2,45 @@ import { useState } from 'react';
 
 const DataEntry = () => {
 
-    const [word, setWord] = useState();
 
+    const [formData, setFormData] = useState({
+        handledValue: '',
+        incomingValue: ''
+    });
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
 
     return (
         <div>
-            <input
-                type="text"
-                value={word}
-                onChange={(e) => setWord(e.target.value)}
-            />
-            <p>{word}</p>
+            <div className='progyny_phones_container'>
+                <h5>Progyny Phones</h5>
+
+                <form onSubmit={handleSubmit} className='progyny_data_row'>
+                    <label>
+                        Handled Calls
+                        <input type='text' name='handledValue' value={formData.handledValue} onChange={(e) => setFormData({
+                            ...formData,
+                            [e.target.name]: e.target.value
+                        })} />
+                    </label>
+
+                    <label>
+                        Incoming Calls
+                        <input type='text' name='incomingValue' value={formData.incomingValue} onChange={(e) => setFormData({
+                            ...formData,
+                            [e.target.name]: e.target.value
+                        })} />
+                    </label>
+                    <button type='submit'>Submit</button>
+                </form>
+
+            </div>
+
+
         </div>
     );
 };
