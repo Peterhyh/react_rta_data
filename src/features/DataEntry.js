02f43fd330290fe6e3716996c86a90b7562db8d4
Peter Huynh <peterhyh@yahoo.com>
2024-01-05 18:11:08 -0600

    form debugging required

--- src/features/DataEntry.js
+++ src/features/DataEntry.js
@@ -3,15 +3,12 @@ import { useState } from 'react';
 const DataEntry = () => {
 
 
-    const [formData, setFormData] = useState({
-        handledValue: '',
-        incomingValue: ''
-    });
-
+    const [handledValue, setHandledValue] = useState('');
+    const [incomingValue, setIncomingValue] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('data:', formData);
+        console.log("test");
     }
 
     return (
@@ -22,21 +19,16 @@ const DataEntry = () => {
                 <form onSubmit={handleSubmit} className='progyny_data_row'>
                     <label>
                         Handled Calls
-                        <input type='text' name='handledValue' value={formData.handledValue} onChange={(e) => setFormData({
-                            ...formData,
-                            [e.target.name]: e.target.value
-                        })} />
+                        <input type='text' name='handledValue' value={handledValue} onChange={(e) => { setHandledValue(e.target.value) }} />
                     </label>
 
                     <label>
                         Incoming Calls
-                        <input type='text' name='incomingValue' value={formData.incomingValue} onChange={(e) => setFormData({
-                            ...formData,
-                            [e.target.name]: e.target.value
-                        })} />
+                        <input type='text' name='incomingValue' value={incomingValue} onChange={(e) => { setIncomingValue(e.target.value) }} />
                     </label>
-                    <button type='submit'>Submit</button>
+
                 </form>
+                <button type='submit'>Submit</button>
 
             </div>
 
