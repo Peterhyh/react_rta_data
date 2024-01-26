0c163f747c5710d19e8e78568c725b113e684cd4
Peter Huynh <peterhyh@yahoo.com>
2024-01-25 19:06:50 -0600

    login page added

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,19 @@
+const LoginPage = () => {
+
+    return (
+        <>
+            <label>
+                Username:
+            </label>
+            <input type='text' />
+
+            <label>
+                Password:
+            </label>
+            <input type='text' />
+
+        </>
+    )
+};
+
+export default LoginPage;
