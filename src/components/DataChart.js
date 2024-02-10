dc61eba8fb357c376315b11434700eb7a076e6bb
Peter Huynh <peterhyh@yahoo.com>
2024-02-09 19:47:47 -0600

    data chart clean up

--- src/components/DataChart.js
+++ src/components/DataChart.js
@@ -1,20 +1,8 @@
 const DataChart = () => {
     return (
-        <table>
-
-            <thead>
-                <tr>
-                    <th>Fertility</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr>
-                    <td></td>
-                </tr>
-            </tbody>
-
-        </table>
+        <div>
+            <p>Test</p>
+        </div>
     )
 }
 
